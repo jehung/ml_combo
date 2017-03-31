78c9be4a4042ad83c24ffcdc0e355373e17fc768
unknown <jehung@me.com>
2017-03-30 23:54:26 -0700

    Add algorithms files

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -3,6 +3,8 @@
 import matplotlib.pyplot as plt
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
+from sklearn import tree
+from sklearn.ensemble import AdaBoostClassifier
 
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
@@ -30,7 +32,11 @@ plt.show()
 
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
+clf = AdaBoostClassifier(tree.DecisionTreeClassifier())
 
+clf.fit(features_train, labels_train)
+
+print clf.score(features_test, labels_test)
 
 
 
--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -12,7 +12,7 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn import svm
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -24,7 +24,24 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+#clf = svm.SVC(kernel='linear')
+clf = svm.SVC(kernel='rbf', C= 10000)
 
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+'''
+t1 = time()
+print(clf.score(features_test, labels_test))
+print "training time:", round(time()-t1, 3), "s"
+'''
+
+pred = clf.predict(features_test)
+print(sum(pred))
 #########################################################
 
 
